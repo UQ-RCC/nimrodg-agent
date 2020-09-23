d739d9852a1bbfddffd9f4557de3bebb45982d92
Zane van Iperen <z.vaniperen@uq.edu.au>
2020-09-23 17:36:21 +1000

    nix: support cross-compilation

--- default.nix
+++ default.nix
@@ -1,13 +1,58 @@
 { nixpkgs ? (import <nixpkgs> {})
+, system ? nixpkgs.hostPlatform
 , useStatic ? true
 }:
-
 let
-  #pkgs = if useStatic then nixpkgs.pkgsStatic else nixpkgs;
-  pkgs = nixpkgs.pkgsStatic;
+  systems = (import <nixpkgs/lib>).systems.examples;
+
+  crossPkgs = (import ~/Documents/Coding/nixpkgs {
+    crossSystem = system;
+    #crossSystem = systems.mingwW64;
+    #crossSystem = systems.musl64;
+  });
+
+  pkgs = if useStatic then crossPkgs.pkgsStatic else crossPkgs;
+
   stdenv = pkgs.stdenv;
 
-  xlibssh2 = pkgs.libssh2.override { openssl = pkgs.libressl; };
+  xlibressl = pkgs.libressl.overrideAttrs(old: rec {
+    # Fixes build issues on Windows
+    cmakeFlags = pkgs.lib.remove "-DENABLE_NC=ON" old.cmakeFlags;
+    outputs    = pkgs.lib.remove "nc" old.outputs;
+  });
+
+  xcurlFull = (pkgs.curlFull.override {
+    openssl        = xlibressl;
+    libssh2        = pkgs.libssh2.override { openssl = xlibressl; };
+    nghttp2        = pkgs.nghttp2.override { openssl = xlibressl; };
+
+    http2Support   = true;
+    idnSupport     = !stdenv.hostPlatform.isWindows;
+    zlibSupport    = true;
+    sslSupport     = true;
+    scpSupport     = true;
+    c-aresSupport  = !stdenv.hostPlatform.isWindows;
+
+    ldapSupport    = false;
+    gnutlsSupport  = false;
+    wolfsslSupport = false;
+    gssSupport     = false;
+    brotliSupport  = false;
+  }).overrideAttrs(old: rec {
+    configureFlags = old.configureFlags ++ [
+      "--disable-file"   "--disable-ldap"  "--disable-ldaps"
+      "--disable-rtsp"   "--disable-proxy" "--disable-dict"
+      "--disable-telnet" "--disable-pop3"  "--disable-imap"
+      "--disable-smb"    "--disable-smtp"  "--disable-cookies"
+      "--disable-openssl-auto-load-config"
+      "--without-ca-bundle" "--without-ca-path"
+    ]
+    ++ stdenv.lib.optionals stdenv.hostPlatform.isWindows [
+      "--with-winidn"
+    ];
+
+    NIX_CFLAGS_COMPILE = stdenv.lib.optionals useStatic ["-DNGHTTP2_STATICLIB"];
+  });
 in
 stdenv.mkDerivation rec {
   name = "nimrodg-agent";
@@ -15,44 +60,15 @@ stdenv.mkDerivation rec {
   nativeBuildInputs = with pkgs; [
     nixpkgs.cmake # Need non-static cmake for this, rhash doesn't like pkgsStatic
     nixpkgs.git
-    binutils      # CMake doesn't find ar without this
     pkgconfig
   ];
 
-  buildInputs = with pkgs; [
-    libressl.dev
-    xlibssh2.dev
+  buildInputs = with pkgs; [ xlibressl.dev xcurlFull.dev ];
 
-    ((curlFull.override {
-      openssl    = libressl;
-      libssh2    = xlibssh2;
-      nghttp2    = pkgs.nghttp2.override { openssl = pkgs.libressl; };
-
-      http2Support   = true;
-      idnSupport     = true;
-      zlibSupport    = true;
-      sslSupport     = true;
-      scpSupport     = true;
-      c-aresSupport  = true;
-
-      ldapSupport    = false;
-      gnutlsSupport  = false;
-      wolfsslSupport = false;
-      gssSupport     = false;
-      brotliSupport  = false;
-    }).overrideAttrs(old: rec {
-      configureFlags = old.configureFlags ++ [
-        "--disable-file"   "--disable-ldap"  "--disable-ldaps"
-        "--disable-rtsp"   "--disable-proxy" "--disable-dict"
-        "--disable-telnet" "--disable-pop3"  "--disable-imap"
-        "--disable-smb"    "--disable-smtp"  "--disable-cookies"
-        "--disable-openssl-auto-load-config"
-        "--without-ca-bundle" "--without-ca-path"
-      ];
-    })).dev
+  cmakeFlags = [
+    "-DNIMRODG_PLATFORM_STRING=${stdenv.hostPlatform.config}"
   ];
 
-  cmakeFlags = [ "-DNIMRODG_PLATFORM_STRING=${stdenv.hostPlatform.config}" ];
   enableParallelBuilding = true;
 
   src = builtins.filterSource (path: type: baseNameOf path != ".git") ./.;
